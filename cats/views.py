e9dab25c95798a419d4ce864f414b968ec59d9a9
Pavel KOlesnikov <practYaPasha@yandex.ru>
2024-02-09 16:38:59 +0300

    1

--- cats/views.py
+++ cats/views.py
@@ -1,13 +1,13 @@
-from rest_framework import generics
+from rest_framework import viewsets 
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-class CatList(generics.ListCreateAPIView):
+class CatViewSet(viewsets.ListCreateAPIView):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
-class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+class CatDetail(viewsets.RetrieveUpdateDestroyAPIView):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
